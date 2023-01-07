8944510d7e279114fb65221935b5af119e2775c0
Sapsan <semya_s@mail.ru>
2023-01-07 21:56:48 +0300

    Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц. Например, даны 2 матрицы: 2 4 | 3 4 3 2 | 3 3 Результирующая матрица будет: 18 20 15 18

---
+++ Task58/Task58/Task58/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+
+
+using System;
+using Task58;
+ArrayWorker worker = new ArrayWorker();
+int[,] array1 = worker.GetArray(2, 2);
+int[,] array2 = worker.GetArray(2, 2);
+int[,] array3 = new int[2, 2];
+worker.PrintArray(array1);
+Console.WriteLine();
+worker.PrintArray(array2);
+Console.WriteLine();
+worker.CompositionMatrix(array1, array2, array3);
+worker.PrintArray(array3);
